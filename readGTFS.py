a35972514eb19a9ef005e1f178b6056daee2048f
Craig O'Donnell <cbodonnell@comcast.net>
2020-02-01 22:30:15 -0500

    joins + njt bus

--- readGTFS.py
+++ readGTFS.py
@@ -3,8 +3,9 @@ from io import StringIO
 import pandas as pd
 
 
-def main():
-    with ZipFile('sample-feed.zip') as zip:
+def zipToDataframes(file_path):
+    with ZipFile(file_path) as zip:
+        dataframes = {}
         for file in zip.filelist:
             file_name = file.filename
             with zip.open(file_name) as f:
@@ -12,10 +13,54 @@ def main():
                 s = str(bytes, 'utf-8')
                 data = StringIO(s)
                 df = pd.read_csv(data)
-                # df = df.transpose()
-                print()
-                print(file_name.split('.txt')[0])
-                print(df)
+                name = file_name.split('.txt')[0]
+                dataframes[name] = df
+                # print()
+                # print(name)
+                # print(df)
+        return dataframes
+
+
+def main():
+    gtfs_dataframes = zipToDataframes('data/njt_bus_gtfs_20200128.zip')
+    agency = gtfs_dataframes['agency']
+    routes = gtfs_dataframes['routes']
+    agency_routes = agency.join(
+        routes.set_index('agency_id'),
+        on='agency_id'
+    )
+    print(agency_routes)
+    trips = gtfs_dataframes['trips']
+    routes_trips = agency_routes.join(
+        trips.set_index('route_id'),
+        on='route_id'
+    )
+    print(routes_trips)
+    route_94_trips = routes_trips.loc[routes_trips['route_short_name'] == '94']
+    stop_times = gtfs_dataframes['stop_times']
+    trips_stop_times = routes_trips.join(
+        stop_times.set_index('trip_id'),
+        on='trip_id'
+    )
+    print(trips_stop_times)
+    calendar_dates = gtfs_dataframes['calendar_dates']
+    trips_calendar_dates = routes_trips.join(
+        calendar_dates.set_index('service_id'),
+        on='service_id'
+    )
+    print(trips_calendar_dates)
+    # frequencies = gtfs_dataframes['frequencies']
+    # trips_frequencies = routes_trips.join(
+    #     frequencies.set_index('trip_id'),
+    #     on='trip_id'
+    # )
+    # print(trips_frequencies)
+    shapes = gtfs_dataframes['shapes']
+    shapes_trips = shapes.join(
+        routes_trips.set_index('shape_id'),
+        on='shape_id'
+    )
+    print(shapes_trips)
 
 
 if __name__ == "__main__":
